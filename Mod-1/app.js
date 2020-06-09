40703ddc66eea0c5bad3c0b921d9a151cb973863
Abrar <soudagar.abrar@gmail.com>
2020-06-09 15:09:30 +0530

    Mod-1-Sol

--- Mod-1/app.js
+++ Mod-1/app.js
@@ -1,15 +1,35 @@
-(function () {
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('LunchCheck', [])
+  angular.module('LunchCheck',[])
 
-.controller('LunchCheckController',LunchCheckController);
-LunchCheckController.$inject=['$scope'];
-function  LunchCheckController($scope){
-  $scope.lunchMenu = "";
-  $scope.foodCheck =0;
-  } 
-});
-
-
-})();
+  .controller('myFirstController', function($scope) {
+    $scope.itemname=" ";
+    $scope.calculateitem = function() {
+      $scope.itemCount=1;
+      $scope.str=" ";
+      var res=0;
+      res=checkLunch($scope.itemname);
+      if($scope.itemname==" ")
+      {
+        $scope.result= "Please Enter Data First";
+      }
+      else if(res>0 && res<=3)
+      {
+        $scope.result= "Enjoy!";
+      }
+      else if(res>3)
+      {
+        $scope.result="Too Much!";
+      }
+    };
+    function checkLunch(string)
+    {
+      var words=[];
+      words=string.split(',');
+      var len=words.length;
+      console.log(len);
+      return len;
+    }
+  });
+}) ();
