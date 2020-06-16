72eed9cab2a33ad315ba06ae89e6c7c5485fabb1
Abrar <soudagar.abrar@gmail.com>
2020-06-16 17:37:36 +0530

    Module-2-soln

--- Mod-2/app.js
+++ Mod-2/app.js
@@ -1,19 +1,19 @@
 (function () {
 'use strict';
 
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyController(ShoppingListCheckOffService) {
-  var toBuy = this;
-  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
-  toBuy.buy = function(index) {
-    ShoppingListCheckOffService.buyItem(index);
-  };
-}
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuy = this;
+    toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+    toBuy.buy = function(index) {
+      ShoppingListCheckOffService.buyItem(index);
+    };
+  }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
@@ -21,30 +21,29 @@ function AlreadyBoughtController(ShoppingListCheckOffService) {
   bought.items = ShoppingListCheckOffService.getBoughtItems();
 
 }
-
 function ShoppingListCheckOffService() {
-  var service = this;
-  var toBuyItems = [
-    { name: "cookies", quantity: 10 },
-    { name: "milk", quantity: 7 },
-    { name: "chips", quantity: 5 },
-    { name: "soda", quantity: 6 },
-    { name: "Final Fantasy XV", quantity: 1 }];
+      var service = this;
+      var toBuyItems = [
+        { name: "cookies", quantity: 10 },
+        { name: "milk", quantity: 7 },
+        { name: "chips", quantity: 5 },
+        { name: "soda", quantity: 6 },
+        { name: "Final Fantasy XV", quantity: 1 }];
 
   var boughtItems = [];
 
-  service.buyItem = function(index) {
-    boughtItems.push(toBuyItems[index]);
-    toBuyItems.splice(index, 1);
-  };
+      service.buyItem = function(index) {
+        boughtItems.push(toBuyItems[index]);
+        toBuyItems.splice(index, 1);
+      };
 
-  service.getToBuyItems = function () {
-    return toBuyItems;
-  };
+      service.getToBuyItems = function () {
+        return toBuyItems;
+      };
 
-  service.getBoughtItems = function () {
-    return boughtItems;
-  };
-}
+      service.getBoughtItems = function () {
+        return boughtItems;
+      };
+    }
 
 })();
